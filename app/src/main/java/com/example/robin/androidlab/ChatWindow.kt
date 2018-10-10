c9b83dce6c1e3558c998c94ee302474b8d6c14f4
Mudr0003 <32398069+Mudr0003@users.noreply.github.com>
2018-10-10 12:09:04 -0400

    start lab 5

--- app/src/main/java/com/example/robin/androidlab/ChatWindow.kt
+++ app/src/main/java/com/example/robin/androidlab/ChatWindow.kt
@@ -2,9 +2,13 @@ package com.example.robin.androidlab
 
 import android.os.Bundle
 import android.app.Activity
+import android.content.ContentValues
 import android.content.Context
 import android.content.Intent
+import android.database.sqlite.SQLiteDatabase
+import android.database.sqlite.SQLiteOpenHelper
 import android.provider.MediaStore
+import android.support.constraint.solver.widgets.Helper
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
@@ -38,6 +42,32 @@ class ChatWindow : Activity() {
 
         var editText = findViewById<EditText>(R.id.EditFieldId)
 
+
+        //db stuff
+        val dbHelper = ChatDatabaseHelper()
+        val db = dbHelper.writableDatabase
+
+
+        val results = db.query(dbHelper.TABLE_NAME, arrayOf(dbHelper.KEY_MESSAGE,dbHelper.KEY_ID),null,null,null,null,null,null)
+
+        var numRows = results.getCount()
+        results.moveToFirst()  //read from first row
+
+        val keyIndex = results.getColumnIndex(dbHelper.KEY_MESSAGE)
+
+        while (!results.isAfterLast)
+        {
+            var thisMessage = results.getString(keyIndex)
+            msgList.add(thisMessage)
+            results.moveToNext()
+
+        }
+
+
+
+
+
+
         Log.i(ACTIVITYNAME, "in onCreate")
 
         setResult(33)
@@ -47,7 +77,20 @@ class ChatWindow : Activity() {
             Log.i(ACTIVITYNAME, "done Chat Button clicked")
 
             if (!editText.text.isEmpty()) {
+
+
             msgList.add(editText.text.toString())}
+
+
+
+            //db stuff
+            var newRow = ContentValues()
+            newRow.put(dbHelper.KEY_MESSAGE, msgList.last())
+
+            db.insert(dbHelper.TABLE_NAME,"",newRow)
+
+
+
 
 
             var theadapter = ChatAdapter(this)
@@ -89,21 +132,18 @@ class ChatWindow : Activity() {
         }
 
 
-
-
-        override fun getView(position : Int, convertView: View?, parent : ViewGroup) : View {
+        override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
 
             var inflater = LayoutInflater.from(parent.context)
 
-            var thisRow:View?
+            var thisRow: View?
 
 
             if (position % 2 == 0) {
 
-              thisRow = inflater.inflate(R.layout.chat_row_incoming, null)
+                thisRow = inflater.inflate(R.layout.chat_row_incoming, null)
 
-        }
-           else {
+            } else {
                 thisRow = inflater.inflate(R.layout.chat_row_outgoing, null)
 
             }
@@ -111,7 +151,7 @@ class ChatWindow : Activity() {
             thisRow.findViewById<TextView>(R.id.message_text_id)
             //var textView = thisRow.findViewById<TextView>(R.id.out_message_text)
 
-            val message = thisRow?.findViewById(R.id.message_text_id)as TextView
+            val message = thisRow?.findViewById(R.id.message_text_id) as TextView
 
             message.text = getItem(position) // get the string at position
 
@@ -121,10 +161,36 @@ class ChatWindow : Activity() {
         override fun getItemId(position: Int): Long {
 
             //  This is the database id of the item at position. For now, we aren’t using SQL, so just return the number: position.
-           // return position as Long
+            // return position as Long
             return 0
         }
 
     }
+
+
+        val DATABASE_NAME = "myfile.db"
+        val VERSION_NUM = 1
+
+        inner class ChatDatabaseHelper : SQLiteOpenHelper(this@ChatWindow, DATABASE_NAME, null, VERSION_NUM){
+
+                val KEY_ID = "_id"
+                val TABLE_NAME = "ChatMessages"
+                val KEY_MESSAGE = "Messages"
+
+                override fun onCreate(db:SQLiteDatabase) {
+                  //  db.execSQL("CREATE_TABLE " + TABLE_NAME + " (" + KEY_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " + KEY_MESSAGE + " TEXT)")
+                    db.execSQL("CREATE TABLE " + TABLE_NAME + " ( _id INTEGER PRIMARY KEY AUTOINCREMENT, " + KEY_MESSAGE + " MESSAGE TEXT)")
+            }
+
+            override fun onUpgrade(db:SQLiteDatabase, oldVersion: Int, newVersion: Int){
+                db.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME)
+                onCreate(db)
+            }
+
+        }
+
+
+
+
 
 }
